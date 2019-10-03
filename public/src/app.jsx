f916fee5fd1c25b62ac239a4eeab95e4f12ffc99
Jerry Fernandez <gmfernandez@cpp.edu>
2019-10-02 21:31:10 -0700

    First commit for React Intranet Sketch.

---
+++ public/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<h1>THIS.IS.SPARTA!</h1>, document.getElementById('app'));
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+//entry -> output 
+const path = require('path');
+
+module.exports = {
+	entry: './public/src/app.jsx',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [{
+			loader: 'babel-loader',
+			test: /\.jsx?$/,
+			exclude: /node_modules/
+		}, {
+		  test: /\.s?css$/,
+		  use: [
+		  	'style-loader',
+		  	'css-loader',
+		  	'sass-loader'
+		  ]	
+		}]
+	},
+	devtool: 'cheap-module-eval-source-map'
+};
+
