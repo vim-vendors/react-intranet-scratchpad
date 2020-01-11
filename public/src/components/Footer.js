6121c3dd1c49af20296f8ac54e49b9b530d901bc
Jerry Fernandez <gmfernandez@cpp.edu>
2020-01-10 19:41:32 -0800

    Upgraded sidebar and started adding icons to page.

--- public/src/components/Footer.js
+++ public/src/components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Footer = (props) => 
 (
 	<footer className="my-footer">
+		<h4>This is the Footer</h4>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 	</footer>
 );
 
--- public/src/components/Sidebar.js
+++ public/src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import SmallContent from './SmallContent'; 
+import userIcon from "../images/interface/png/user-3.png";
+import placeholder from "../images/interface/png/placeholder.png";
+import supportIcon from "../images/interface/png/speech-bubble-1.png";
+import magGlass from "../images/interface/png/zoom-in.png";
+
 const Sidebar = (props) => 
 (
 	<aside className="sidebar">
@@ -7,18 +12,28 @@ const Sidebar = (props) =>
 		<nav>
 			<ul>
 				<li>
-					<a href="#">Lady Pic</a>
+					<a href="#">
+						<img src={userIcon} alt=""/>
+					</a>
 				</li>
 				<li>
-					<a href="#">Star Icon</a>
+					<a href="#">
+						<img src={placeholder} alt=""/>
+					</a>
 				</li>
 				<li>
-					<a href="#">Support Icon</a>
+					<a href="#">
+						<img src={supportIcon} alt=""/>
+					</a>
 				</li>
 			</ul>
 		</nav>
-		
-		<p>Search Bar</p>
+		<div class="search-container">
+		  <form action="">
+		    <input type="text" placeholder="Search.." name="search"/>
+		    <button type="submit"><img src={magGlass} alt=""/></button>
+		  </form>
+		</div>
 		<SmallContent/>
 		<SmallContent/>
 		<SmallContent/>
