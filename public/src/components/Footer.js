52a4e3a980648e96e8900aac74f972c56101c950
Jerry Fernandez <gmfernandez@cpp.edu>
2020-01-14 00:31:04 -0800

    Adjusted the colors to match the original mockup color scheme.

--- public/src/components/Footer.js
+++ public/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = (props) => 
 (
 	<footer className="my-footer">
-		<h4>Temporary Footer</h4>
-		<div>Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-		<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+		<h1>Footer</h1>
+		<div className="credit-links">Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+		<div className="credit-links">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 	</footer>
 );
 
