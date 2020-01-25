30e0957b1a5fd51803f6d7ca5ae80f5444a97aa9
Jerry Fernandez <gmfernandez@cpp.edu>
2020-01-24 21:53:13 -0800

    Added media query mixins for multiple devices.

--- public/src/components/SideContent.js
+++ public/src/components/SideContent.js
@@ -6,14 +6,14 @@ import placeholder from "../images/interface/png/placeholder.png";
 const SideContent = (props) => 
 (
 	<div>
-		<aside className="side_content">
+{/*		<aside className="side_content">
 			<p>Side Content - Filler</p>
 			<li><a href="#"><img src={placeholder} alt="Placeholder"/></a></li>
 		</aside>
 		<aside className="side_content">
 			<p>Side Content - Filler</p>
 			<li><a href="#"><img src={placeholder} alt="Placeholder"/></a></li>
-		</aside>		
+		</aside>*/}		
 	</div>
 
 	
